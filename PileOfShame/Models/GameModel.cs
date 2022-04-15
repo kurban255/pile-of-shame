ab4b84faa5dc4808bf919ce717607ba34f9bd3d4
karol <krl.urbanski@gmail.com>
2022-04-16 00:48:51 +0200

    styled filters frontend

--- PileOfShame/Models/GameModel.cs
+++ PileOfShame/Models/GameModel.cs
@@ -6,11 +6,13 @@ namespace PileOfShame.Models
     public class GameModel
     {
         public int ID { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Genre { get; set; }
         public string Platform { get; set; }
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
     }
 }
