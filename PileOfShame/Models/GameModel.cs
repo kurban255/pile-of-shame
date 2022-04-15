38edd4c9833dc42f48bcf9a264494142c64bdbfd
kurban255 <krl.urbanski@gmail.com>
2022-04-15 20:59:36 +0200

    Add files via upload

---
+++ PileOfShame/Models/GameModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PileOfShame.Models
+{
+    public class GameModel
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Platform { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
